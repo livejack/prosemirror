bf42579fed9b02bae8d619a3accd663a4b4f3ed0
Jérémy Lal <kapouer@melix.org>
2021-01-03 12:29:27 +0100

    Reexports needed symbols

---
+++ src/index.js
@@ -0,0 +1,28 @@
+export { keymap } from "prosemirror-keymap";
+export { history, undo, redo } from "prosemirror-history";
+export {
+	baseKeymap,
+	chainCommands,
+	toggleMark,
+	exitCode,
+	joinUp,
+	joinDown,
+	lift,
+	selectParentNode,
+	wrapIn,
+	setBlockType
+} from "prosemirror-commands";
+export { EditorState, Plugin, NodeSelection, TextSelection } from "prosemirror-state";
+export { dropCursor } from "prosemirror-dropcursor";
+export { gapCursor } from "prosemirror-gapcursor";
+export {
+	inputRules, wrappingInputRule,
+	smartQuotes, emDash, ellipsis,
+	undoInputRule
+} from "prosemirror-inputrules";
+
+export { EditorView } from "prosemirror-view";
+export { Schema, DOMParser, DOMSerializer } from "prosemirror-model";
+export { addListNodes, wrapInList, splitListItem } from "prosemirror-schema-list";
+
+export { default as crel } from "crelt";
